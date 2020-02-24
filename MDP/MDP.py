99641b60b81209f7a8d86743012afc19aab1d593
El Shar <IJE8@pitt.edu>
2020-02-23 22:28:10 -0500

    added step and reset methods to the MDP class

--- MDP/MDP.py
+++ MDP/MDP.py
@@ -15,9 +15,10 @@ License
     The MIT License  
 """
 
-from spaces.space import  Space
-from transition import Transition
-from objective import Objective
+from MDP.spaces.space import  Space
+from MDP.transition import Transition
+from MDP.objective import Objective
+import copy
 
 class MDP:
     """
@@ -29,30 +30,42 @@ class MDP:
     
     """
     
-    def __init__(self,   sSpace = None,
+    def __init__(self,   initState = None,
+                         sSpace = None,
                          aSpace = None,
+                         nSpace = None,
                          transition = None,
                          objective  = None,
-                         ifFiniteHorizon = False,
-                         isAveCost = False):
+                         isFiniteHorizon = False,
+                         isAveCost = False,
+                         terminalStates=None,):
         
         """
         Inputs
         ------
+            initState [list]: initial state vector, that is the list of
+                              components of the starting state.
+        
             sSpace [Space]: MDP state space.
 
             aSpace [Space]: MDP action space.  
+            
+            nSpace [Space]: MDP exogenous noise space.  
             
             transition [Transition]: MDP stochastic kernel (e.g., MDP 
                                      transition matrix for discrete MDPs).
             
             objective [Objective]: the MDP cost/reward function.
             
-            ifFiniteHorizon [bool]: if True, MDP is finite-time horizon,
-                                    else it is infinite-time horizon.
+            isFiniteHorizon [int]: if int, MDP is finite-time horizon of length
+                                   isFiniteHorizon, else if False,
+                                   it is infinite-time horizon.
             
             isAveCost [bool]: if True, MDP is average-cost, else it is
-                              discounted-cost.    
+                              discounted-cost. 
+            
+            terminalStates [list]: list of absorbing state for episodic MDPs
+                                   
                               
         Raises/Returns
         --------------
@@ -62,21 +75,107 @@ class MDP:
             The constructor of MDP class.
         """
         
-        assert(isinstance(sSpace,Space))
-        assert(isinstance(aSpace,Space))
+        # assert(isinstance(sSpace,Space))
+        # assert(isinstance(aSpace,Space))
+        # assert(isinstance(nSpace,Space))
         assert(isinstance(transition,Transition))
-        assert(isinstance(objective,Objective))        
+        assert(isinstance(objective,Objective))  
+        assert sSpace.isStateFeasble(initState), 'Intial state should belong to\
+                                                  the state space'
         
+        self.initState = initState
+        self.terminalStates = terminalStates
         self.sSpace = sSpace
         self.aSpace = aSpace
+        self.nSpace = nSpace
         self.sDim = self.sSpace.dim
-        self.aDim = self.aSpace.dim        
+        self.aDim = self.aSpace.dim
+        self.nDim = self.nSpace.dim          
         self.transition = transition
         self.objective = objective
-        self.isFiniteHorizon = ifFiniteHorizon
+        self.isFiniteHorizon = isFiniteHorizon
         self.isAveCost = isAveCost
+        self.reset()
+        
+        
+    def step(self, action, force_noise=None):     
+        '''
+        Takes one step in the MDP.
+        --------------------------
+        Inputs
+        ------
+        action [list]: current action vector, that is the list of
+                       components of the current action
+           
+        force_noise [list]: optional, an exogenous noise vector used to 
+                            evaluate next state and reward. If not provided,
+                            the noise vector will be sampled randomly
+                            
+        Returns
+        -------
+        nextState [list]: next state at t+1
+            
+        reward [float]: Scalar reward/cost
+            
+        done [boolean]: True if an absorbing state is reached,
+                        for the case of absorbing MDPs
+                        
+        info [dict]: Provides info about the noise outcome and current period 
+                    in the finite horizon case
+            
+        '''
+        if not force_noise:
+            noise = self.nSpace.sample()[0]
+        else:
+            noise = force_noise
+            
+        nextState = self.transition.getNextStateWithExoSamples(self.currState,
+                                                               action,
+                                                               noise)
+        reward = self.objective.getObjectiveWithExoSamples(self.currState,
+                                                      action,
+                                                      noise)
+        self.currState = nextState
+        if self.isFiniteHorizon:
+            # Increment the period
+            self.t += 1
+            if self.t >= self.isFiniteHorizon:
+                self.reset()
+            return nextState, reward, {'t': self.t, 'noise': noise}
+        
+        # Infinite horizon MDP    
+        elif self.terminalStates:
+            done = nextState in self.terminalStates
+            return nextState, reward, done, {'noise': noise}            
+        else:
+            return nextState, reward, {'noise': noise} 
+        
+        
+    def reset(self,):
+        '''
+        Resets the state back to the initial state
+        ------------------------------------------
+        
+        Returns
+        -------
+        initState [list]: initial state vector, that is the list of
+                          components of the starting state.
+                          
+        t [int]: starting period t for finit horizon MDPs           
+                              
+        '''
+        self.currState = copy.deepcopy(self.initState)
+        if self.isFiniteHorizon:
+            self.t = 0
+            return (self.currState,self.t)
+        else:
+            return self.currState
+            
+            
+        
  
-       
+
+           
 
     
         
